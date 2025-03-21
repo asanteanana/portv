73c5ce2e03e58c7bd4c547ad5ad6041aa165e8d3
wwwnana <nanaasante76@gmail.com>
2025-03-21 03:03:10 -0500

    fix: remove blog references from sitemap and RSS

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -1,42 +1,16 @@
-import { baseUrl } from 'app/sitemap'
-import { getBlogPosts } from 'app/blog/utils'
+import RSS from 'rss'
+import { baseUrl } from '../sitemap'
 
 export async function GET() {
-  let allBlogs = await getBlogPosts()
+  const feed = new RSS({
+    title: 'Nana Asante',
+    site_url: baseUrl,
+    feed_url: `${baseUrl}/feed.xml`,
+  })
 
-  const itemsXml = allBlogs
-    .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-        return -1
-      }
-      return 1
-    })
-    .map(
-      (post) =>
-        `<item>
-          <title>${post.metadata.title}</title>
-          <link>${baseUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ''}</description>
-          <pubDate>${new Date(
-            post.metadata.publishedAt
-          ).toUTCString()}</pubDate>
-        </item>`
-    )
-    .join('\n')
-
-  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
-    <channel>
-        <title>My Portfolio</title>
-        <link>${baseUrl}</link>
-        <description>This is my portfolio RSS feed</description>
-        ${itemsXml}
-    </channel>
-  </rss>`
-
-  return new Response(rssFeed, {
+  return new Response(feed.xml({ indent: true }), {
     headers: {
-      'Content-Type': 'text/xml',
+      'Content-Type': 'application/xml',
     },
   })
 }
